511d4528794b504f870ed403bd3166fa256f45fb
sailesh kumar <skumar@petz.net>
2024-07-22 21:51:58 +0530

    fixed interest and message views and tested apis

--- chatapp/app/views.py
+++ chatapp/app/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from app.models import Interest, Message
@@ -31,10 +31,12 @@ class RegisterAPIView(APIView):
         })
         
 class InterestsAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    
     def get(self, request):
         user = request.user
         try:
-            interests = Interest.objects.filter(user=user, accepted=False)
+            interests = Interest.objects.filter(sender=user, accepted=False)
             serializer = InterestSerializer(interests, many=True)
             return Response(data=serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
@@ -44,15 +46,24 @@ class InterestsAPIView(APIView):
     def post(self, request):
         data = request.data
         sender = request.user
-        receiver_id = data.get('receiver_id')
+        receiver_id = data.get('receiver')
         try:
             receiver = User.objects.get(id=receiver_id)
-            serializer = InterestSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(data = serializer.data, status=status.HTTP_200_OK)
-            
-        except User.DoesNotExist:
+            interest = Interest.objects.filter(sender=sender, receiver=receiver).get()
+            if not interest:
+                data['sender']= sender.id
+                serializer = InterestSerializer(data=data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(data = serializer.data, status=status.HTTP_200_OK)
+            else:
+                interest.status = "sent"
+                interest.accepted=False
+                interest.save()
+                serializer = InterestSerializer(interest)
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(f"Error: {e}")
             return Response({"error":"Receiver DoesNotExist"}, status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request):
@@ -64,32 +75,35 @@ class InterestsAPIView(APIView):
             interest.accepted = True
             interest.save()
             return Response(status=status.HTTP_200_OK)
-        except Interest.DoesNotExist:
+        except Interest.DoesNotExist as e:
             return Response({"error":"Interest DoesNotExist"}, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request):
         interest_id = request.data.get('interest_id')
         try:
-            interest = interest = Interest.objects.get(d=interest_id)
-            interest.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            interest = Interest.objects.get(id=interest_id)
+            interest.status = "canceled"
+            interest.save()
+            serializer = InterestSerializer(interest)
+            return Response(data = serializer.data, status=status.HTTP_201_CREATED)
         except Interest.DoesNotExist as e:
             print(f"error: {e}")
             return Response({"error":"Interest DoesNotExist"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MessagesAPIView(APIView):
+    permission_classes = [IsAuthenticated]
     
     def get(self, request):
         data = request.GET
         user = request.user
-        receiver_id = data.get("receiver_id")
+        receiver_id = data.get("receiver")
         if not receiver_id:
-            raise Exception(f"Receiver is required")
+            return Response({"error":"Receiver ID is required"}, status=status.HTTP_400_BAD_REQUEST)
         try:
             receiver = User.objects.get(id=receiver_id)
-            messages = Message.objects.filter(sender=user, receiver=receiver)
-            serializer = MessageSerializer(data=messages, many=True)
+            messages = Message.objects.filter(sender=user, receiver=receiver).exclude(status="deleted")
+            serializer = MessageSerializer(messages, many=True)
             return Response(data=serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             print(f"error:{e}")
@@ -97,15 +111,16 @@ class MessagesAPIView(APIView):
     
     def post(self, request):
         data = request.data
-        user = request.user
-        receiver_id = data.get("receiver_id")
+        sender = request.user
+        receiver_id = data.get("receiver")
         if not receiver_id:
-            raise Exception(f"Receiver is is required")
+            return Response({"error":"Receiver ID is required"}, status=status.HTTP_400_BAD_REQUEST)
         try:
-            serializer = MessageSerializer(data=data, many=True)
+            data['sender'] = sender.id
+            serializer = MessageSerializer(data=data)
             if serializer.is_valid():
-                serializer.save(sender=user)
+                serializer.save()
                 return Response(data=serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             print(f"error:{e}")
-            return Response(data={}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error":"Unable to sent message"}, status=status.HTTP_400_BAD_REQUEST)
