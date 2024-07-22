c8e8252a2baea931316a8d73593e772a2241578c
sailesh kumar <skumar@petz.net>
2024-07-22 15:12:34 +0530

    added models

--- chatapp/app/models.py
+++ chatapp/app/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Interest(models.Model):
+    sender = models.ForeignKey(User, related_name='interests_sent', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='interests_received', on_delete=models.CASCADE)
+    message = models.TextField()
+    accepted = models.BooleanField("Accepted", default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.sender}"
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.sender}"
