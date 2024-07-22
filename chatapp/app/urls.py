ea5245b79e35624c6edfce23cd1996e07a0fb289
sailesh kumar <skumar@petz.net>
2024-07-22 15:14:16 +0530

    urls and views

--- chatapp/app/urls.py
+++ chatapp/app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
+from app.views import (InterestsAPIView, MessagesAPIView,)
 
 
 urlpatterns = [
+    path('interests/', InterestsAPIView.as_view(), name='interests'),
+    path('messages/', MessagesAPIView.as_view(), name='messages'),
     path('token/', TokenObtainPairView.as_view(), name='access_token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
 ]
