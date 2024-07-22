03d8387018326abe2e1538b490b15bf51d3a48c3
sailesh kumar <skumar@petz.net>
2024-07-22 14:54:04 +0530

    added urls to login

---
+++ chatapp/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
+
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='access_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+]
