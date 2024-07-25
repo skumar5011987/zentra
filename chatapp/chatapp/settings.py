4d76b9bbff677adb0de4e0fa513c02b1faa5006e
sailesh kumar <skumar@petz.net>
2024-07-25 20:43:23 +0530

    updated settings

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -40,18 +40,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'daphne',
     'django.contrib.staticfiles',
+    
     "corsheaders",
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
-    'app',
     'channels',
+    
+    'app',
 ]
 
 ASGI_APPLICATION = 'chatapp.asgi.application'
